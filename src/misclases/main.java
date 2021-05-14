c93ae229fa1f31e765452353e9b23dc91b49ed50
Adrian Marquez <>
2021-05-13 20:39:40 -0500

    Base para Logeo del Programa

--- src/misclases/main.java
+++ src/misclases/main.java
@@ -12,6 +12,6 @@ package misclases;
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hola a todos");
     }
 }
